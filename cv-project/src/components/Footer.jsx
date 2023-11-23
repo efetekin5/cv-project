4ae6006a133aeaaa718990ea94782d7f3af931d6
efe <121581223+efetekin5@users.noreply.github.com>
2023-11-23 13:11:42 +0300

    finish footer

---
+++ cv-project/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import '../styles/footer.css'
+import github from '../../public/github.png' 
+
+export default function Footer() {
+    return(
+        <div className='footer'>
+            <h1 className='madeBy'>Made by Efe Tekin</h1>
+            <a target='blank' href='https://github.com/efetekin5'>
+                <img className='gitHubIcon' src={github}></img>
+            </a>
+        </div>
+    )
+}
--- cv-project/src/main.jsx
+++ cv-project/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import Header from './components/Header.jsx'
 import './styles/global.css'
 import Content from './components/Content.jsx'
+import Footer from './components/Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header></Header>
     <Content></Content>
+    <Footer></Footer>
   </React.StrictMode>,
 )
